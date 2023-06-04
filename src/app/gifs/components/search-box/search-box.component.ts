25a278ca8fa8b33c0e2ece1e75a6f57e0992d987
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-03 22:18:06 -0600

    gifsService

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -10,9 +11,12 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) { }
+
   searchTag():void{
     const newTag: string = this.tagInput.nativeElement.value;
-    console.log({newTag});
-
+    // console.log({newTag});
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
   }
 }
