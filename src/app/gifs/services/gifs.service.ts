047ef15b22e49cb27e6ee8bd1c90be3200a6c6aa
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-03 23:13:06 -0600

    http request w HttpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,15 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey:       string = 'VEY4EdXlQ1YT7ORruCZN6OnpPrnfNikC';
   private _tagsHistory: string[] = [];
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -29,5 +31,19 @@ export class GifsService {
     if(tag.length === 0) return;
 
     this.organizeHistory(tag);
+
+    // fetch(`https://api.giphy.com/v1/gifs/search?api_key=VEY4EdXlQ1YT7ORruCZN6OnpPrnfNikC&q=colorfull&limit=10`)
+    // .then(resp => resp.json())
+    // .then(data => console.log(data));
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search`, {params})
+    .subscribe(resp => {
+      console.log(resp);
+    });
   }
 }
