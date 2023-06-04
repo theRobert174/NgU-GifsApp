b77ab75ceee3fa1ffacb6f4a7235ee980cf64c7d
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-03 21:19:53 -0600

    basic structure n components

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
+  exports: [
+    HomePageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GifsModule { }
