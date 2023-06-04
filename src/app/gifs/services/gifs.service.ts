79dfc4d3ebd6de847c2eb95de15db875d0bd1f26
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-03 22:26:58 -0600

    service validations

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,21 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  public searchTag(tag: string): void {
+  private organizeHistory(tag: string){
+    tag = tag.toLowerCase();
+
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
+  public searchTag(tag: string): void {
+
+    if(tag.length === 0) return;
+
+    this.organizeHistory(tag);
   }
 }
