2dd0280c3d64b4df89f4a9bb5b003b7d6c5237ee
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-03 23:51:14 -0600

    show gifs in screen

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -10,8 +10,12 @@ export class SidebarComponent {
 
   constructor(private gifsService: GifsService) { }
 
-  get tags() {
+  get tags(): string[] {
     return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string) : void{
+    this.gifsService.searchTag(tag);
   }
 
 }
